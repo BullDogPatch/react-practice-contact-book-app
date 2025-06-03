72cfe2c015b35baf3e07b437388726ec4d013587
Craig Clayton <craig__clayton@outlook.com>
2025-06-03 17:01:12 +0100

    messing with styling, need to get functionaltiy working first

--- src/App.tsx
+++ src/App.tsx
@@ -25,14 +25,15 @@ function App() {
   const [contacts, setContacts] = useState<SingleContact[]>(initialContacts);
 
   return (
-    <>
-      <h2 className='text-3xl font-bold'>Contact Book</h2>
-      <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+    <div className='m-auto'>
+      <h2 className='text-left text-3xl font-bold'>Contact Book</h2>
+      <p className='text-left'>Keep track of your friends live.</p>
+      <div className='mt-6 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         {contacts.map((contact) => (
           <Person key={contact.id} contact={contact} />
         ))}
       </div>
-    </>
+    </div>
   );
 }
 
