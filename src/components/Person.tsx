20ece7cc057d4e9b5b56d3cbb7fc8c1747ba207d
Craig Clayton <craig__clayton@outlook.com>
2025-06-03 18:12:33 +0100

    got edit mode working, just need to add function to save

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import type { SingleContact } from '../App';
 
 interface Props {
@@ -5,15 +6,55 @@ interface Props {
 }
 
 const Person = ({ contact }: Props) => {
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [editedName, setEditedName] = useState(contact.name);
+  const [editedCity, setEditedCity] = useState(contact.city);
+
   return (
     <div className='card card-dash bg-base-100 w-full'>
       <div className='card-body'>
-        <p className='text-2xl'>{contact.name}</p>
-        <p className='text-xl'>{contact.city}</p>
+        {isEditMode ? (
+          <input
+            type='text'
+            className='input'
+            value={editedName}
+            onChange={(e) => setEditedName(e.target.value)}
+          />
+        ) : (
+          <p className='text-2xl'>{contact.name}</p>
+        )}
+        {isEditMode ? (
+          <input
+            type='text'
+            className='input'
+            value={editedCity}
+            onChange={(e) => setEditedCity(e.target.value)}
+          />
+        ) : (
+          <p className='text-xl'>{contact.city}</p>
+        )}
         <div className='card-actions justify-end'>
-          <button className='px-4 py-2 bg-gray-500 text-white rounded-md'>
-            Edit
-          </button>
+          {isEditMode ? (
+            <div>
+              <button
+                onClick={() => setIsEditMode(false)}
+                className='px-3 py-2 bg-red-500 text-white rounded-md cursor-pointer mr-2'
+              >
+                Cancel
+              </button>
+
+              <button className='px-3 py-2 bg-green-500 text-white rounded-md cursor-pointer'>
+                Save
+              </button>
+            </div>
+          ) : (
+            <button
+              className='px-4 py-2 bg-gray-500 text-white rounded-md cursor-pointer'
+              onClick={() => setIsEditMode(true)}
+            >
+              Edit
+            </button>
+          )}
         </div>
       </div>
     </div>
